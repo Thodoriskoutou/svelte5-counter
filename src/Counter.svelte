4f003aef9c61f792c8fe6fd226a2e2ae27d236f6
thodoris <th.koutouritos@gmail.gr>
2025-06-24 11:29:53 +0300

    upgrated to svelte5

--- src/Counter.svelte
+++ src/Counter.svelte
@@ -1,49 +1,49 @@
 <script>
-import { onMount } from 'svelte';
 
-export let values, duration, random, minspeed;
-let counterResult = {};
-let init = false;
-var timers = [];
+let {values, duration, random, minspeed} = $props()
+let counterResult = $state({})
+let init = $state(false)
+var timers = []
 
-onMount(() => {
-    duration = parseInt(duration) || 5000;
-    minspeed = parseInt(minspeed) || 20;
+$effect(() => {
+    duration = parseInt(duration) || 5000
+    minspeed = parseInt(minspeed) || 20
     random = (random == 'true');
     for (let [key, value] of Object.entries(values)) {
-        let step = 1;
-        let max = parseInt(value);
-        let randomMax = Math.floor(max / 2);
-        let randomMin = 0;
+        let step = 1
+        let max = parseInt(value)
+        let randomMax = Math.floor(max / 2)
+        let randomMin = 0
         while((duration / (max / step)) < minspeed){
-            step++;
+            step++
         }
-        counterResult[key] = 0;
+        counterResult[key] = 0
         timers[key] = setInterval(function(){
             if(counterResult[key] < max){
                 if(random){
-                    randomMin = Math.floor(counterResult[key] / 2);
-                    counterResult[key] = getRandomInt(randomMin, randomMax);
-                    randomMax += step;
+                    randomMin = Math.floor(counterResult[key] / 2)
+                    counterResult[key] = getRandomInt(randomMin, randomMax)
+                    randomMax += step
                 }
-                counterResult[key] += step;
+                counterResult[key] += step
             } else {
-                counterResult[key] = max;
-                clearInterval(timers[key]);
+                counterResult[key] = max
+                clearInterval(timers[key])
             }
-        }, duration / (max / step));
+        }, duration / (max / step))
     }
-    init = true;
-});
+    init = true
+})
 
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min
 }
 
 </script>
 
+<!-- svelte-ignore slot_element_deprecated -->
 {#if init}
 <slot {counterResult}></slot>
 {/if}
